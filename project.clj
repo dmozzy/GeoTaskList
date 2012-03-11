e04108f1dad9e4d949ea6583eb981d818a4428c1
Daniel Moscufo <daniel@moscufo.id.au>
2012-03-11 12:26:14 +0800

    tutorial #1 code

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject GeoTaskList "1.0.0-SNAPSHOT"
+ :description "Geolocated Tasklist mobile application"
+  :dependencies [[org.clojure/clojure "1.3.0"] [cheshire "2.2.0"] [compojure "1.0.1"]]
+  :dev-dependencies [[appengine-magic "0.4.7"]])
