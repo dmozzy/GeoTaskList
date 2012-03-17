04c852cdb15314d9e93f84d1f8add2d2ca3fb211
Daniel Moscufo <daniel@moscufo.id.au>
2012-03-17 22:29:30 +0800

    This is the code on completion of #2 of the blog posts

--- src/GeoTaskList/core.clj
+++ src/GeoTaskList/core.clj
@@ -3,7 +3,9 @@
             [compojure.route :as route])
   (:use [compojure.core] 
         [cheshire.core :as json] 
-        [ring.util.response :as ring-response]))
+        [ring.util.response :as ring-response]
+        [compojure.handler :as comp-handler]
+        [GeoTaskList.persistence :as persist]))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
@@ -11,20 +13,27 @@
    :body (json/generate-string data)})
 
 
-(def test-location-1 {:name "Hardware" :lat -31.957242 :lng 115.931422})
-(def test-location-2 {:name "Mall" :lat -31.964779 :lng 115.935241})
-(def test-locations (list test-location-1 test-location-2))
-
 (defroutes geotasklist-main-handler
+           ;Get All Locations Routes
            (GET "/locations" []
-                (json-response {:locations test-locations}))
+                (json-response {:locations (get-all-locations)}))
+
+           ;Delete Location Route           
+           (POST "/delete-location" {params :params} 
+                 (json-response (delete-location (Integer/parseInt (:locationId params)))))
+
+           ;Update Location Route                                 
+           (POST "/location" {params :params} 
+                 (json-response {:locationId (persist/save-location params)}))                         
            
-           (GET "/location/:id" [id]
-                (json-response (nth test-locations id)))
+           ;Static Resources
            (route/resources "/")
            (route/not-found "Page not found"))
 
-(ae/def-appengine-app geotasklist-app #'geotasklist-main-handler)
+
+(def app (comp-handler/api geotasklist-main-handler))
+
+(ae/def-appengine-app geotasklist-app (var app))
 
 (comment
   (ae/serve geotasklist-app)
